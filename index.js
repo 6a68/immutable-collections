a7972e5be92e4420ca51be3a2ed5dd388d9883f0
Isaac Wagner <isaacbwagner@gmail.com>
2013-10-15 14:46:14 -0400

    Created stubs for data structures

---
+++ index.js
@@ -0,0 +1,6 @@
+exports.Vector = require('./lib/vector');
+exports.List   = require('./lib/list');
+exports.Queue  = require('./lib/queue');
+exports.Stack  = require('./lib/stack');
+exports.Map    = require('./lib/map');
+exports.Set    = require('./lib/set');
