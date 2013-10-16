5ffec6d31fbfab362c444d5c085232e246a872d7
Isaac Wagner <isaacbwagner@gmail.com>
2013-10-16 14:43:13 -0400

    Revert "Renamed Queue to BankersQueue"
    This reverts commit c915f72f925ae9d04c872ec93db1b517a5a26409.

--- test/queueSpec.js
+++ test/queueSpec.js
@@ -1,5 +1,5 @@
 var assert = require("chai").assert,
-    Queue = require("../index").BankersQueue;
+    Queue = require("../lib/queue");
 
 describe("Queue", function() {
   it("should start out empty", function() {
